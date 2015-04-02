13653173825fc59f18f85fc6d8ede66b633ca6c4
Eric Bonnet <eric.d.bonnet@gmail.com>
2015-04-02 15:28:45 +0200

    Update

--- R/NaviCell_RC.R
+++ R/NaviCell_RC.R
@@ -23,6 +23,7 @@ NaviCell <- setRefClass(
             proxy_url <<- "https://navicell.curie.fr/cgi-bin/nv_proxy.php"
             map_url <<- "https://navicell.curie.fr/navicell/maps/cellcycle/master/index.php"
             msg_id <<- 1000
+            session_id <<- ""
         }
     )
 )
@@ -56,6 +57,24 @@ NaviCell$methods(
     }
 )
 
+#' Set a given zoom level on associated NaviCell map in browser
+#'
+#' @param module map module name (optional, empty string "" for current map)
+#' @param zoom_level value 
+#' @export
+NaviCell$methods(
+    setZoom = function(module, zoom_level) {
+        list_param <- list(module=module, args = array(zoom_level), msg_id = .self$msg_id, action = 'nv_set_zoom')
+        .self$incMessageId()
+        str_data <- .self$makeData(.self$formatJson(list_param))
+        #print(str_data)
+        response <- postForm(.self$proxy_url, style='POST', id = .self$session_id, mode='cli2srv', perform='send_and_rcv', data=str_data, .opts=curlOptions(ssl.verifypeer=F))
+        response <- .self$formatResponse(response)
+        message(response)
+    }
+)
+
+
 #' Launch client browser and points to the default NaviCell map 
 #' 
 #' @param None 
@@ -63,15 +82,75 @@ NaviCell$methods(
 #' @export
 NaviCell$methods(
     launchBrowser = function(...) {
-    .self$incMessageId()
-    if (.self$session_id == "") {
-        .self$generateSessionId()
+        .self$incMessageId()
+        if (.self$session_id == "") {
+            .self$generateSessionId()
+        }
+        url <- paste(.self$map_url, '?id=', .self$session_id, sep = '')
+        browseURL(url)
+    }
+)
+
+#' List all NaviCell server sessions.
+#'
+#' @param None
+#' @return None
+#' @export 
+NaviCell$methods(
+    listSessions = function(...) {
+        response <- postForm(.self$proxy_url, style='POST', id="1", perform="list", msg_id = .self$msg_id, mode="session", .opts=curlOptions(ssl.verifypeer=F))
+        response <- .self$formatResponse(response)
+        message(response)
+    }
+)
+
+#' Attach a NaviCell server session ID 
+#'
+#' @param session_id a valid NaviCell session ID (e.g. returned by the function listSessions) 
+#' @return None
+#' @export 
+NaviCell$methods(
+    attachSession = function(session_id) {
+        if (.self$session_id != "") {
+            warning("Session id already set.")
+            return()
+        }
+        .self$incMessageId()
+        # check session id on NaviCell server
+        response <- postForm(.self$proxy_url, style='POST', id = session_id, msg_id = .self$msg_id, mode = 'session', perform = 'check', .opts=curlOptions(ssl.verifypeer=F))
+        response <- .self$formatResponse(response)
+        if (response == "ok") {
+            .self$session_id <- session_id
+        }
+        else {
+            warning("Wrong session id.")
+        }
     }
 )
 
 
-#' Format session ID response to appropriate value 
+#' Attach NaviCell handle to the last existing NaviCell Web Service session.
+#'
+#' @param
+#' @return
+#' @export
+NaviCell$methods(
+    attachLastSession = function(...) {
+        response <- postForm(.self$proxy_url, style='POST', id = '1', msg_id = .self$msg_id, mode = 'session', perform = 'get', which = '@@', .opts=curlOptions(ssl.verifypeer=F))
+        response <- .self$formatResponse(response)
+        message(response)
+        .self$attachSession(response)
+    }
+)
+
+
+
+
+
+
+#' Format response obtained from the RCurl 'postForm' command 
 #' 
+#' In some cases return from postForm call is 'raw' (e.g. R session in a terminal) otherwise it's plain text (e.g. R GUI)
 #' @param None 
 #' @return None 
 NaviCell$methods(
@@ -87,5 +166,26 @@ NaviCell$methods(
     }
 )
 
+#' Create NaviCell server command string from a list of parameters 
+#' 
+#' @param json string 
+#' @return NaviCell command string 
+NaviCell$methods(
+    makeData = function(json_string) {
+         ret <- paste("@COMMAND ", json_string, sep = "")
+         return(ret)
+    }
+)
 
-
+#' Format list of parameters to NaviCell server compatible JSON format
+#'
+#' @param list of parameters
+#' @return json formatted string
+NaviCell$methods(
+    formatJson = function(list_param) {
+        data <- toJSON(list_param)
+        data <- gsub("\n", '', data)
+        data <- gsub(" ", "", data)
+        return(data)
+    }
+)
